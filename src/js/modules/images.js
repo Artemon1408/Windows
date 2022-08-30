bccbfa502b18481ff76f2965990c61e1744e07f7
Artem Drozhzhyn <12Artemondrozhzhyn@gmail.com>
2022-08-30 19:00:17 +0300

    first commit

---
+++ src/js/modules/images.js
@@ -0,0 +1,40 @@
+const images = () => {
+    const imgPopup = document.createElement('div'),
+          workSection = document.querySelector('.works'),
+          bigImage = document.createElement('img');
+
+    imgPopup.classList.add('popup');
+
+    workSection.appendChild(imgPopup);
+
+    imgPopup.style.justifyContent = 'center';
+    imgPopup.style.alignItems = 'center';
+    imgPopup.style.display = 'none';
+
+    imgPopup.appendChild(bigImage);
+
+    workSection.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let target = e.target;
+
+        if (target && target.classList.contains('preview')) {
+            document.body.style.overflow = 'hidden';
+            imgPopup.style.display = 'flex';
+            bigImage.style.cssText = `
+                width: 500px;
+                height: 500px;
+            `;
+
+
+            const path = target.parentNode.getAttribute('href');
+            bigImage.setAttribute('src', path);
+        }
+
+        if (target && target.matches('div.popup')) {
+            imgPopup.style.display = 'none';
+        }
+    });
+};
+
+export default images;
